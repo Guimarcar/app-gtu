3532ac610b8f0bfffc58d9535f5d937b6c73da7e
Guilherme Marques <guimarcar02@gmail.com>
2020-07-22 00:06:18 -0300

    adicionado firebase

--- src/app/core/components/primeiro/primeiro.component.ts
+++ src/app/core/components/primeiro/primeiro.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
 @Component({
-  templateUrl: './primeiro.component.html',
-  styleUrls: ['./primeiro.component.css']
+  templateUrl: "./primeiro.component.html",
+  styleUrls: ["./primeiro.component.css"],
 })
 export class PrimeiroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
