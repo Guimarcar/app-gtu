a28cf6f0e5ab72be555a9e8e277ee633d4a625b2
Guilherme Marques <guimarcar02@gmail.com>
2020-07-27 20:44:18 -0300

    adicionado inputs

--- src/app/core/components/primeiro/primeiro.component.ts
+++ src/app/core/components/primeiro/primeiro.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { AngularFireAuthModule } from "@angular/fire/auth";
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @Component({
-  templateUrl: "./primeiro.component.html",
-  styleUrls: ["./primeiro.component.css"],
+  templateUrl: './primeiro.component.html',
+  styleUrls: ['./primeiro.component.css'],
 })
 export class PrimeiroComponent implements OnInit {
+  private isPassword = true;
+  public eye = 'visibility';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  alternaTipo() {
+    if (this.isPassword) {
+      this.eye = 'visibility_off';
+      this.isPassword = false;
+    } else {
+      this.eye = 'visibility';
+      this.isPassword = true;
+    }
+  }
+
+  get TipoCampo(): string {
+    if (this.isPassword) {
+      return 'password';
+    } else {
+      return 'text';
+    }
+  }
 }
